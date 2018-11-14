de7f736a06a15afb36f509951bde4c719b75bbcb
pokachopotun <mr.salixnew@gmail.com>
2018-11-14 04:15:22 +0300

    [upd] небольшой дебаг. вроде бы работает

--- blackholes/main.cpp
+++ blackholes/main.cpp
@@ -9,30 +9,30 @@ using namespace std;
 using graph_t = vector< vector< int > * >;
 using blackhole_t = vector< int >;
 
-graph_t parseGraphFile(const string& filePath){
-	graph_t g, g_rev;
+pair< graph_t, graph_t > parseGraphFile(const string& filePath){
+	graph_t g, gRev;
 	ifstream gin(filePath);
 	int n, m;
 	gin >> n >> m;
 	for(int i = 0; i < n; i++){
 		g.push_back(new vector<int>());
-		g_rev.push_back(new vector<int>());
+		gRev.push_back(new vector<int>());
 	}
 	for(int i= 0; i < m; i++){
 		int x,y;
-		cin >> x >> y;
+		gin >> x >> y;
 		g[x]->push_back(y);
-		g_rev[y]->push_back(x);
+		gRev[y]->push_back(x);
 	}
-	return g;
+	return make_pair(g, gRev);
 }
 
 vector<int> getSubtree(const graph_t& g, int s){
 	vector<int> ans;
 	queue<int> q;
 	q.push(s);
-	vector< char > used(g.size(), 0);
-	used[s] = 1;
+	vector< bool > used(g.size());
+	used[s] = true;
 	while(!q.empty()){
 		int v = q.front();
 		q.pop();
@@ -48,10 +48,11 @@ vector<int> getSubtree(const graph_t& g, int s){
 	return ans;
 }
 
-void dye(const graph_t& g, int s, vector<int>& used, int color){
+void weakConnectivity(int s, const graph_t& g, const graph_t& gRev, vector<int>& used){
+	
 	queue<int> q;
 	q.push(s);
-	used[s] = color;
+	used[s] = s;
 	while(!q.empty()){
 		int v = q.front();
 		q.pop();
@@ -59,10 +60,20 @@ void dye(const graph_t& g, int s, vector<int>& used, int color){
 			if( g[to] == nullptr ){
 				continue;
 			}
-			if(used[to] > 0){
-				used[to];
+			if(used[to] >= 0){
+				continue;
 			}
-			used[to] = color;
+			used[to] = s;
+			q.push(to);
+		}
+		for(int to : *gRev[v]){
+			if( gRev[to] == nullptr ){
+				continue;
+			}
+			if(used[to] >= 0 ){
+				continue;
+			}
+			used[to] = s;
 			q.push(to);
 		}
 	}
@@ -89,18 +100,17 @@ graph_t* getReverseSubGraph(const graph_t& gRev, const graph_t& sub){
 	return res;
 }
 
-vector< blackhole_t > findBlackHoles(const graph_t& g, int n){
+vector< blackhole_t > findBlackHoles(const graph_t& g, const graph_t& gRev, int n){
 	n++;
 	vector< blackhole_t > ans;
 	graph_t* potential = nullptr, *potentialPrev = nullptr, *potentialRev = nullptr;
 	for( int bHoleSize = 1; bHoleSize < n; bHoleSize++){
-		
+		cout << bHoleSize << endl;
 
 		// build potential list
-		delete potentialPrev;
 		potentialPrev = potential;
 		potential = getSubgraph(g, bHoleSize);
-		potentialRev = getReverseSubGraph(g, *potential);
+		potentialRev = getReverseSubGraph(gRev, *potential);
 
 		//select candidate list
 		for(int v = 0; v < potential->size(); v++){
@@ -108,7 +118,8 @@ vector< blackhole_t > findBlackHoles(const graph_t& g, int n){
 				continue;
 			}
 			for(int to : *( (*potential)[v] ) ){
-				if((*potentialPrev)[v] != nullptr && (*potential)[to] == nullptr){
+				if( (potentialPrev == nullptr || 
+					( potentialPrev != nullptr && (*potentialPrev)[v] == nullptr )) && (*potential)[to] == nullptr){
 					vector< int >  subTree = getSubtree(*potentialRev, v);
 					for(int rm : subTree){
 						(*potential)[rm] = (*potentialRev)[rm] = nullptr;
@@ -117,12 +128,12 @@ vector< blackhole_t > findBlackHoles(const graph_t& g, int n){
 			}
 		}
 
-		//select candidate list
+		//select final candidate list
 		for(int v = 0; v < potential->size(); v++){
 			if((*potential)[v] == nullptr){
 				continue;
 			}
-			vector< int >  subTree = getSubtree(*potentialRev, v);
+			vector< int >  subTree = getSubtree(*potential, v);
 			if( subTree.size() > bHoleSize ){
 				for(int rm : subTree){
 					(*potential)[rm] = (*potentialRev)[rm] = nullptr;
@@ -135,9 +146,45 @@ vector< blackhole_t > findBlackHoles(const graph_t& g, int n){
 				}
 			}
 		}
+		vector<int> used(g.size(), -1);
+		for(int v = 0; v < potential->size(); v++){
+			if((*potential)[v] == nullptr){
+				continue;
+			}
+			if(used[v] == -1){
+				weakConnectivity(v, *potential, *potentialRev, used);
+			}
+		}
+		cout << bHoleSize << " Used:" << endl;
+		for(int i=0; i < used.size(); i++){
+			cout << i << " " << used[i] << endl;
+		}
+		cout << "<<<<<<<<<<<" << endl;
+		for(int i = 0; i < potential->size(); i++){
+			vector< int > comp;
+			for(int v = 0; v < potential->size(); v++){
+				if(used[v] == i){
+					comp.push_back(v);
+				}
+			}
+			// if(comp.size() == bHoleSize){
+				ans.push_back(comp);
+			// }
+		}
 	}
 	return ans;
-}	
+}
+
+void printOutput(const vector< blackhole_t >& ans, const string& outputFilePath){
+	ofstream out(outputFilePath);
+	for(auto vv : ans){
+		out << vv.size() << " ";
+		for(auto v : vv){
+			out << v << " ";
+		}
+		out << endl;
+	}
+}
 int main(int argc, char* argv[]){
 	if(argc < 3){
 		cout << "use ./blackholes input.txt output.txt" << endl;
@@ -146,6 +193,10 @@ int main(int argc, char* argv[]){
 	const string& inputFilePath(argv[1]);
 	const string& outputFilePath(argv[2]);
 
+	auto g = parseGraphFile(inputFilePath);
+	cout << "Parsing done" << endl;
+	auto ans = findBlackHoles(g.first, g.second, g.first.size());
 
+	printOutput(ans, outputFilePath);
 	return 0;
 }
