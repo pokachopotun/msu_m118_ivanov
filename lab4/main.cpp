dd3312f3a8725d9db7ab310f51ec8ca20c189f04
pokachopotun <mr.salixnew@gmail.com>
2018-11-20 01:17:25 +0300

    [upd] второй режим

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -37,7 +37,7 @@ int main(int argc, char* argv[]){
 
 
 	printf("rank %d h %d w %d \n", rank, h, w);
-	if(true){
+	if( w <= h ){
 		int hLocal = h / size; 
 		int wLocal = w;
 		int bwLocal = w / size;
@@ -49,16 +49,6 @@ int main(int argc, char* argv[]){
 
 		MPI_File_set_view(AinputFile, 8, MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
 		MPI_File_read_at_all(AinputFile, 8 * localSize * rank, A, localSize, MPI_DOUBLE, MPI_STATUS_IGNORE);
-
-		// double * tmp = new double[h * w];
-		// for(int i =0 ;i < h; i++){
-		// 	for(int j = 0; j < w; j++){
-		// 		cout << tmp[i * w + j] << " ";
-		// 	}
-		// 	cout << endl;
-		// }
-
-		// return 0;
 
 		for(int i = 0; i < bwLocal; i++){
 			C[i]  = 0.0;
@@ -72,13 +62,6 @@ int main(int argc, char* argv[]){
 
 			MPI_File_set_view(BinputFile, 8, MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
 			MPI_File_read_at_all(BinputFile, 8 * curRank * bwLocal, B, bwLocal, MPI_DOUBLE, MPI_STATUS_IGNORE);
-
-			if(rank == 0){
-				for(int i =0 ;i < bwLocal; i++){
-					cout << B[i] << " ";
-				}
-				cout << endl;
-			}
 
 			int colStartLocal = curRank * bwLocal;
 			int colEndLocal = colStartLocal + bwLocal;
@@ -103,7 +86,61 @@ int main(int argc, char* argv[]){
 		MPI_File_set_view(CoutputFile, 8, MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
 		MPI_File_write_at_all(CoutputFile, 8 * rank * bwLocal, C, bwLocal, MPI_DOUBLE, MPI_STATUS_IGNORE);
 	}else{
+		int hLocal = h; 
+		int wLocal = w / size;
+		int bwLocal = w / size;
+		int localSize = hLocal * wLocal;
 
+		double* A = new double[localSize];
+		double* B = new double[bwLocal];
+		double* C = new double[bwLocal];
+		double* C1 = new double[bwLocal];
+
+		MPI_File_set_view(AinputFile, 8, MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
+		for(int row = 0; row < h; row++ ){
+			MPI_File_read_at_all(AinputFile, 8 * ( row * w + wLocal * rank ), &A[wLocal * row], wLocal, MPI_DOUBLE, MPI_STATUS_IGNORE);
+		}
+		MPI_File_set_view(BinputFile, 8, MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
+		MPI_File_read_at_all(BinputFile, 8 * rank * bwLocal, B, bwLocal, MPI_DOUBLE, MPI_STATUS_IGNORE);
+
+		MPI_File_set_view(CoutputFile, 0, MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
+		if(rank == 0){
+			int outRows = h;
+			int outCols = 1;
+			MPI_File_write_at(CoutputFile, 0, &outRows, 1, MPI_INTEGER, MPI_STATUS_IGNORE);
+			MPI_File_write_at(CoutputFile, 4, &outCols, 1, MPI_INTEGER, MPI_STATUS_IGNORE);
+		}
+		MPI_File_set_view(CoutputFile, 8, MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
+
+		for(int i = 0; i < bwLocal; i++){
+			C[i]  = 0.0;
+		}
+
+		int rowStartLocal = rank * hLocal;
+		int rowEndLocal = rowStartLocal + hLocal;
+
+		for( int iter =0 ; iter < size; iter++ ){
+			MPI_Barrier(MPI_COMM_WORLD);
+			for(int i = 0; i < bwLocal; i++){
+				C[i] = C1[i] = 0.0;
+			}
+			int rowStartLocal = iter * bwLocal;
+			int rowEndLocal = rowStartLocal + bwLocal;
+
+			for(int row = rowStartLocal; row < rowEndLocal; row++){
+				for(int col = 0; col < wLocal; col++ ){
+					int aIndex = row * wLocal + col;
+					int cIndex = row - rowStartLocal;
+					int bIndex = col;
+					C[cIndex] += B[bIndex] * A[aIndex];
+				}
+			}
+			MPI_Barrier(MPI_COMM_WORLD);
+			MPI_Reduce(C, C1, bwLocal, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+			if( rank == 0 ){
+				MPI_File_write_at(CoutputFile, 8 * iter * bwLocal, C1, bwLocal, MPI_DOUBLE, MPI_STATUS_IGNORE);
+			}
+		}
 	}
 
 	MPI_Finalize();
