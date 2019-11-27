559b409e1401ae7cfb5cef5c673c3973f01a353a
pokachopotun <mr.salixnew@gmail.com>
2019-11-27 08:24:45 +0300

    chinese algo works!

--- blackholes/chinese/main.cpp
+++ blackholes/chinese/main.cpp
@@ -5,12 +5,13 @@
 #include <set>
 #include <queue>
 #include <stack>
+#include <algorithm>
 
 
 using namespace std;
 
 using TGraph = vector<vector<int>>;
-using TClosure = vector<int>;
+using TClosure = set<int>;
 using TCandidates = set<int>;
 using TUsed = vector<char>;
  
@@ -18,13 +19,13 @@ class Solution {
 public:
 
     static TGraph ReadGraphFromFile(const string& filename) {
-        ifstream fin(filename, std::ifstream::in);
+        ifstream fin(filename);
         int n, m;
-        TGraph graph(n);
         fin >> n >> m;
+        TGraph graph(n);
         for(int i = 0; i < m; i++) {
             int x, y;
-            cin >> x >> y;
+            fin >> x >> y;
             graph[x].push_back(y);
         }
         return graph;
@@ -81,21 +82,25 @@ public:
         return ClosureBFS(g, used, v);
     }
 
-    static int DegreeOut(const TClosure& closure, const TGraph& gd, const TGraph& gr) {
+    static int DegreeOut(const TClosure& closure, const TGraph& gd) {
         long long dout = 0;
         for (int v : closure) {
-           dout += static_cast<long long>(gd[v].size()) - static_cast<long long>(gr[v].size());
+            for (int to : gd[v]) {
+                bool vin = closure.find(v) != closure.end();
+                bool toin = closure.find(to) != closure.end();
+                dout += vin && !toin;
+            }
         }
         return dout;
-    } 
+    }
 
     static bool HasCandidates(const TClosure& closure, const TCandidates& candidates) {
         for (int v : closure) {
-            if (candidates.find(v) != candidates.end()) {
-                return true;
+            if (candidates.find(v) == candidates.end()) {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
 private:
@@ -149,7 +154,7 @@ private:
         if (!used[v]) {
             q.push(v);
             used[v] = 1;
-            closure.push_back(v);
+            closure.insert(v);
         }
         while(!q.empty()) {
             v = q.front();
@@ -158,7 +163,7 @@ private:
                 if (!used[to]) {
                     used[to] = 1;
                     q.push(to);
-                    closure.push_back(v);
+                    closure.insert(to);
                 }
             }
         }
@@ -167,80 +172,129 @@ private:
 
 };
 
+void Print(const string& heading, const set<int>& closure) {
+    cout << heading << ": ";
+    for (int v : closure) {
+        cout << v << " ";
+    }
+    cout << endl;
+}
+
 int main(int argc, char** argv) { 
     const string inputFileName(argv[1]);
-    const int maxBHSize = atoi(argv[2]);
+    int maxBHSize = atoi(argv[2]);
 
     {
-
-        int maxBHSize;
 
         TGraph graph = Solution::ReadGraphFromFile(inputFileName);
         TGraph graphRev = Solution::GetReverseGraph(graph);
         TGraph graphUndir = Solution::GetUndirectedGraph(graph);
-        
+
+        cerr << "Graph built" << endl;
+
+        if (maxBHSize == -1) {
+            maxBHSize = graph.size();
+        } else {
+            maxBHSize = min(maxBHSize, static_cast<int>(graph.size()));
+        }
+ 
         vector<TCandidates> candidates(maxBHSize + 1);
         for (int i = 1; i <= maxBHSize; i++) {
+            cerr << "Iter: " << i << endl;
             for (int v = 0; v < graph.size(); v++) {
                 if (graph[v].size() < i) {
                     candidates[i].insert(v);
                 }
             }
-
+            Print("P", candidates[i]);
+            
             TCandidates toRemove;
             for (int v : candidates[i]) {
                 if (candidates[i - 1].find(v) != candidates[i - 1].end()) {
                     continue;
                 }
                 TClosure closure = Solution::GetClosure(graph, v);
-                if (Solution::HasCandidates(closure, candidates[i])) {
+                if (!Solution::HasCandidates(closure, candidates[i])) {
                     TClosure revClosure = Solution::GetClosure(graphRev, v);
-                    for (int x : revClosure) {
-                        toRemove.insert(x);
-                    }
+                    toRemove.insert(revClosure.begin(), revClosure.end());
                 } 
             }
 
             for (int v : toRemove) {
                 auto it = candidates[i].find(v);
                 if (it != candidates[i].end()) {
-                    candidates[i].erase(v);
+                    candidates[i].erase(it);
                 }
             }
             toRemove.clear();
+            Print("C", candidates[i]);
     // ---------------------------------------------
             for (int v : candidates[i]) {
-                int cs = Solution::ClosureSize(graph, v);
+                TClosure closure = Solution::GetClosure(graph, v);
+                size_t cs = closure.size();
                 if (cs >= i) {
+                    TClosure revClosure = Solution::GetClosure(graphRev, v);
+                    toRemove.insert(revClosure.begin(), revClosure.end());
                     if (cs == i) {
                         // closure v is a blackhole
+                        Print("BH_C", closure);
                     }
-                    TClosure closure = Solution::GetClosure(graphRev, v);
-                    toRemove.insert(closure.begin(), closure.end());
                 }
             }
 
             for (int v : toRemove) {
                 auto it = candidates[i].find(v);
                 if (it != candidates[i].end()) {
-                    candidates[i].erase(v);
+                    candidates[i].erase(it);
                 }
             }
+
             toRemove.clear();
+            Print("F", candidates[i]);
     // ---------------------------------------------
-            TUsed used;
-            used.resize(graph.size());
-            for (int v = 0; v < candidates[i].size(); v++) {
-                if (used[v]) {
-                    continue;
+            {
+    
+                TUsed used;
+                used.assign(graph.size(), 1);
+                for (int v : candidates[i]) {
+                    used[v] = 0;
                 }
-                TClosure closure = Solution::GetClosure(graphUndir, v, used);
-                size_t cs = closure.size();
-                int dout = Solution::DegreeOut(closure, graph, graphRev);
-                if (cs == i && dout == 0) {
-                    // restricted closure of v is blackhole
+                for (int v : candidates[i]) {
+                    if (used[v]) {
+                        continue;
+                    }
+                    TClosure closure = Solution::GetClosure(graphUndir, v, used);
+                    size_t cs = closure.size();
+                    int dout = Solution::DegreeOut(closure, graph);
+                    if (cs == i && dout == 0) {
+                        // restricted closure of v is blackhole
+                        Print("BH_F", closure);                    
+                    }
                 }
             }
+/*
+    // ---------------------------------------------
+            cerr << "Going undir" << endl;
+            {
+                TUsed used;
+                used.assign(graph.size(), 1);
+                for (int v : candidates[i]) {
+                    used[v] = 0;
+                }
+                for (int v : candidates[i]) {
+                    if (used[v]) {
+                        continue;
+                    }
+                    TClosure closure = Solution::GetClosure(graphUndir, v, used);
+                    closure.push_back(v);
+                    size_t cs = closure.size();
+                    int dout = Solution::DegreeOut(closure, graph, graphRev);
+                    if (cs == i && dout == 0) {
+                        // restricted closure of v is blackhole
+                        Print("BH", closure);                    
+                    }
+                }
+            } */
         }
     }
 
