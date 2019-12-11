26052104da81320569569014e1d03dcbd61ed198
pokachopotun <mr.salixnew@gmail.com>
2019-12-11 19:17:46 +0300

    results and report v1

---
+++ hpc_mod_and_tech/lab2/results_bluegene.py
@@ -0,0 +1,63 @@
+import os
+import sys
+
+def sc(x):
+    return '{:.2e}'.format(x)
+
+if __name__ == "__main__":
+    
+    resFile = sys.argv[1]
+
+    base = dict()
+    with open(resFile, 'r') as file:
+        for line in file:
+            a = line.strip().split()
+
+            pc = a[0]
+            nodes = int(a[1])
+            grid = int(a[2])
+            omp = int(a[4])
+            time = float(a[6])
+            delta = float(a[14]) 
+
+            if nodes == 128 and omp == 1:
+                base[grid] = time
+
+
+
+    with open(resFile, 'r') as file:
+        delim = " & "
+        s = "HPC" + delim 
+        s += "Nodes" + delim
+        s += "Grid" + delim
+        s += "OMP Threads" + delim
+        s += "Wall time" + delim
+        s += "Delta" + delim
+        s += "Speedup" + delim
+        s += "Efficiency" + r" \\"
+        print(s)
+        for line in file:
+            a = line.strip().split()
+            pc = a[0]
+            nodes = int(a[1])
+            grid = int(a[2])
+            omp = int(a[4])
+            time = float(a[6])
+            delta = float(a[14]) 
+            speedup = base[grid] / time
+            expected = (nodes / 128) * omp
+            efficiency = speedup  / expected
+
+            lst = [pc, nodes, grid, omp, time, delta, speedup, efficiency]
+            delim = " & "
+            s = ""
+            s += pc + delim 
+            s += str(nodes) + delim
+            s += str(grid) + delim
+            s += str(omp) + delim
+            s += sc(time) + delim
+            s += sc(delta) + delim
+            s += sc(speedup) + delim
+            s += sc(efficiency) + r" \\"
+
+            print(s)
