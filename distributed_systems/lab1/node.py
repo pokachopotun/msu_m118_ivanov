22578cea952498ac0fd052aa93c431b2ab4f8fd4
pokachopotun <mr.salixnew@gmail.com>
2020-03-03 18:40:09 +0300

    Lab1 works!

--- distributed_systems/lab1/node.py
+++ distributed_systems/lab1/node.py
@@ -1,92 +1,204 @@
 import sys
 import pika
+import datetime
+import time
+from threading import Thread, Lock
+
+def Log(idx, message):
+    print("{} [id = {}] {}".format(datetime.datetime.now(), idx, message))
+
+def LogMethod(idx, typename, method):
+    Log(idx, "[{}] {}".format(typename, method))
 
 class TMsg:
+    def __init__(self, msg_type = "NONE", sender = -1, receiver = -1, numbers = []):
+        self.msg_type = msg_type
+        self.sender = sender
+        self.receiver = receiver
+        self.numbers = numbers
+
     def ToString(self):
-        serialized = "{} {}".format(self.msg_type, self.sender)
+        serialized = "{} {} {}".format(self.msg_type, self.sender, self.receiver)
         for x in self.numbers:
-            serialized += str(x)
+            serialized += " {}".format(x)
         return serialized
 
     def FromString(self, serialized):
         data = serialized.split(' ')
         self.msg_type = data[0]
         self.sender = int(data[1])
-        self.numbers = data[2:]
+        self.receiver = int(data[2])
+        if len(data) >= 4:
+            self.numbers = [int(x) for x in data[3:]]
+        else:
+            self.numbers = []
 
-def queue_name(from_id, to_id):
-    return "queue_from_{}_to_{}".format(from_id, to_id)
 
-class TNode:
+class TRabbitClient:
+    def __init__(self, idx):
+        self.idx = idx
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.input = self.connection.channel()
+        self.output = self.connection.channel()
+
+    def QueueName(self, to_id):
+        return "queue_to_{}".format(to_id)
+
+    def SendMessage(self, message, to_id):
+        #LogMethod(self.idx, "TRabbitClient", "SendMessage")
+        target = self.QueueName(to_id)
+        self.output.queue_declare(queue=target)
+        self.output.basic_publish(exchange='', routing_key=target, body=message)
+
+    def GetNextMessageForId(self, recipient_id, timeout_seconds = 5):
+        #LogMethod(self.idx, "TRabbitClient", "GetNextMessageForId")
+        target = self.QueueName(recipient_id)
+        self.input.queue_declare(queue=target)
+        frame, properties, body = next(self.input.consume(queue=target, inactivity_timeout=timeout_seconds))
+        if frame and properties and body:
+            self.input.basic_ack(frame.delivery_tag)
+            return body.decode()
+        LogMethod(self.idx, "ERROR", "LEADER FAILED")
+        return None
+
+class TProcessor:
     def __init__(self, ring_size, idx, leader):
+        self.mutex = Lock()
         self.ring_size = ring_size
         self.idx = idx
         self.leader = leader
-        self.nxt = GetNextNode(self.idx)
-        self.last_leader_message = -1
+        self.nxt = self.GetNextNode(self.idx)
+        self.prev = -1
 
-    def SendNext(self, message):
-        message.sender = self.idx
-        # TODO:
-        pass
+    def Ping(self):
+        LogMethod(self.idx, "TProcessor", "Ping")
+        self.mutex.acquire()
+        msg = TMsg("PING", self.idx, self.nxt, [])
+        self.mutex.release()
+        return msg
 
-    def RecvNext(self):
-        # TODO:
-        pass
+    def RecvNext(self, serialized):
+        LogMethod(self.idx, "TProcessor", "RecvNext Message: {}".format(serialized))
+        if serialized:
+            return self.ProcessMessage(serialized)
+        else:
+            if self.prev == self.leader or self.prev == -1:
+                return self.InitiateElection()
+        return None
 
     def ProcessMessage(self, serialized):
-        msg = TMsg()
+        LogMethod(self.idx, "TProcessor", "ProcessMessage")
+        message = TMsg()
         message.FromString(serialized)
         if message.msg_type == "ELECTION":
-            ProcessElection(message)
+            return self.ProcessElection(message)
         if message.msg_type == "LEADER":
-            ProcessLeader(message)
-        if message.msg_type == "DISCOVER":
-            ProcessDiscover(message)
+            return self.UpdateLeader(message)
+        if message.msg_type == "PING":
+            return self.ProcessPing(message)
         raise Exception("[{}] Unknown message type! Serialized message: \"{}\"".format(idx, serialized))
 
     def GetNextNode(self, cur):
+        LogMethod(self.idx, "TProcessor", "GetNextNode")
         return (cur + 1) % self.ring_size
 
+    def InitiateElection(self):
+        LogMethod(self.idx, "TProcessor", "InitiateElection")
+        LogMethod(self.idx, "ELECTION", "START")
+        return TMsg("ELECTION", self.idx, self.nxt, [self.idx])
+
     def ProcessElection(self, message):
-        if leader == self.nxt:
-            self.nxt = GetNextNode(self.nxt)
-        if idx in message.numbers:
-            leader = max(message.numbers)
-            LeaderSelected(leader)
+        LogMethod(self.idx, "TProcessor", "ProcessElection")
+        if self.leader == self.nxt and self.leader != self.idx:
+            self.mutex.acquire()
+            self.nxt = self.GetNextNode(self.nxt)
+            self.mutex.release()
+        if self.idx in message.numbers:
+            return self.SelectLeader(message)
         else:
-            message.numbers.append(self.idx)
-            SendNext(message)
+            return self.AddSelfAndForward(message)
 
-    def LeaderSelected(self, leader):
-        msg = TMsg()
-        msg.msg_type = "LEADER"
-        msg.numbers = [self.idx, leader]
-        SendNext(msg)
+    def AddSelfAndForward(self, message):
+        LogMethod(self.idx, "TProcessor", "AddSeldAndForward")
+        message.receiver = self.nxt
+        message.numbers.append(self.idx)
+        LogMethod(self.idx, "ELECTION", "ADD SELF AND FORWARD MESSAGE")
+        return message
 
-    def ProcessLeader(self, message):
-        initiator, self.leader = message.numbers
+    def SelectLeader(self, message):
+        LogMethod(self.idx, "TProcessor", "SelectLeader")
+        self.leader = max(message.numbers)
+        LogMethod(self.idx, "ELECTION", "NEW LEADER {}".format(self.leader))
+        return TMsg("LEADER", self.idx, self.nxt, [self.idx, self.leader])
+
+    def UpdateLeader(self, message):
+        LogMethod(self.idx, "TProcessor", "UpdateLeader")
+        initiator, new_leader = message.numbers
+        if new_leader != self.leader:
+            self.leader = new_leader
+            LogMethod(self.idx, "ELECTION", "NEW LEADER {}".format(self.leader))
         if initiator != self.idx:
-            SendNext(message)
+            message.receiver = self.nxt
+            return message
+        LogMethod(self.idx, "ELECTION", "END")
+        return None
 
-    def ProcessDiscover(self, message):
-        if idx in message.numbers:
-            leader = max(message.numbers)
-            LeaderSelected(leader)
-        else:
-            message.numbers.append(self.idx)
-            SendNext(message)
+    def ProcessPing(self, message):
+        LogMethod(self.idx, "TProcessor", "ProcessPing")
+        self.prev = message.sender
+        return None
 
+class TReader(Thread):
+    def __init__(self, node):
+        Thread.__init__(self)
+        self.client = TRabbitClient(node.idx)
+        self.node = node
+
+    def run(self):
+        while True:
+            if self.node.idx != 9:
+                serialized = self.client.GetNextMessageForId(self.node.idx)
+                response = self.node.RecvNext(serialized)
+                if response:
+                    self.client.SendMessage(response.ToString(), response.receiver)
+
+class TPinger(Thread):
+    def __init__(self, node):
+        Thread.__init__(self)
+        self.client = TRabbitClient(node.idx)
+        self.node = node
+
+    def run(self):
+        while True:
+            if self.node.idx != 9:
+                message = self.node.Ping()
+                self.client.SendMessage(message.ToString(), message.receiver)
+            time.sleep(1)
+
+class TNode:
+    def __init__(self, ring_size, idx, leader):
+        self.idx = idx
+        self.node = TProcessor(ring_size = ring_size, idx = idx, leader = leader)
+
+        self.pinger = TPinger(node = self.node)
+        self.reader = TReader(node = self.node)
+
+        Log(self.idx, "Initialized!")
+
+    def start(self):
+        Log(self.idx, "Started!")
+        self.pinger.start()
+        self.reader.start()
 
 if __name__ == "__main__":
-    if (len(sys.argv) < 1):
-        print("Use python3 node.py idx")
-    idx = int(sys.argv[1])
-    print("Started node index {}".format(idx))
-    print(queue_name(1,2))
-#    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-#    channel = connection.channel()
-#    channel.queue_declare(queue='hello')
-#    channel.basic_publish(exchange='', routing_key='hello', body='hello_world')
-#    print("message sent")
-#    connection.close()
+    if (len(sys.argv) < 3):
+        print("Use: python3 node.py ring_size leader")
+        exit(0)
+
+    ring_size = int(sys.argv[1])
+    leader = int(sys.argv[2])
+
+    nodes = [TNode(ring_size = ring_size, idx = i, leader = leader) for i in range(ring_size)]
+
+    for node in nodes:
+        node.start()
---
+++ distributed_systems/lab1/run.sh
@@ -0,0 +1,1 @@
+timeout 20s python3 node.py 10 9 > out.txt
