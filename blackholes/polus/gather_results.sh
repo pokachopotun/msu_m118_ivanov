2b93fd77a007bf1e8803999c551b4254445c2f27
Denis Ivanov <mr.salixnew@gmail.com>
2020-05-09 05:54:41 +0300

    fix issue in gather_results.sh

--- blackholes/polus/gather_results.sh
+++ blackholes/polus/gather_results.sh
@@ -4,7 +4,7 @@
 
 grep -n "It took me" outputs/*.short > time.txt
 grep -n "bh_count" outputs/*.short > bh_count.txt
-grep -n "It took me" outputs/*.short > bh_filtered.txt
+grep -n "bh_filtered" outputs/*.short > bh_filtered.txt
 
 resdir=../results/$(basename $(pwd))
 mkdir $resdir
