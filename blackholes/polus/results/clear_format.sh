ce6d8466507b76b77a219d8898eee843aabef495
pokachopotun <mr.salixnew@gmail.com>
2020-06-02 04:42:15 +0300

    clear_format.sh

--- blackholes/polus/results/clear_format.sh
+++ blackholes/polus/results/clear_format.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sed -i 's/.short:.:It took me://g' time.txt
-sed -i 's/.short:.:bh_count//g' bh_count.txt
-sed -i 's/.short:.:bh_filtered//g' bh_filtered.txt
-sed -i 's/outputs\///g' *.txt
+folder=$1
+
+sed -i 's/.short:.*:It took me://g' $folder/time.txt
+sed -i 's/.short:.*:bh_count//g' $folder/bh_count.txt
+sed -i 's/.short:.*:bh_filtered//g' $folder/bh_filtered.txt
+sed -i 's/outputs\///g' $folder/*.txt
